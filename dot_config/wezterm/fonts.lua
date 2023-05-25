07e19f14c31503ec092ef8c02f75a5f9f6840d45
Brian Bonsignore <bbonsign@users.noreply.github.com>
2023-05-25 00:59:41 -0400

    simplify wezterm font config

--- dot_config/wezterm/fonts.lua
+++ dot_config/wezterm/fonts.lua
@@ -3,34 +3,34 @@ return {
 
   font = wezterm.font({ family = 'FiraCode Nerd Font', weight = "Medium", harfbuzz_features = { 'zero' } }),
 
-  font_rules = {
-    {
-      intensity = 'Bold',
-      italic = true,
-      font = wezterm.font {
-        family = 'VictorMono Nerd Font',
-        weight = 'Bold',
-        style = 'Italic',
-      },
-    },
-    {
-      italic = true,
-      intensity = 'Half',
-      font = wezterm.font {
-        family = 'VictorMono Nerd Font',
-        weight = 'Bold',
-        style = 'Italic',
-      },
-    },
-    {
-      italic = true,
-      intensity = 'Normal',
-      font = wezterm.font {
-        family = 'VictorMono Nerd Font',
-        style = 'Italic',
-        weight = "Bold"
-      },
-    },
-  },
+--  font_rules = {
+--   {
+--     intensity = 'Bold',
+--     italic = true,
+--     font = wezterm.font {
+--       family = 'VictorMono Nerd Font',
+--       weight = 'Bold',
+--       style = 'Italic',
+--     },
+--   },
+--   {
+--     italic = true,
+--     intensity = 'Half',
+--     font = wezterm.font {
+--       family = 'VictorMono Nerd Font',
+--       weight = 'Bold',
+--       style = 'Italic',
+--     },
+--   },
+--   {
+--     italic = true,
+--     intensity = 'Normal',
+--     font = wezterm.font {
+--       family = 'VictorMono Nerd Font',
+--       style = 'Italic',
+--       weight = "Bold"
+--     },
+--   },
+-- },
 
 }
