6e5d8f8e2bcfe64bb089f0388382a2756a69ed81
Brian Bonsignore <bbonsign@users.noreply.github.com>
2023-07-05 00:35:49 -0400

    lvim: switch to nvim-surround

--- dot_config/lvim/lua/bb/mappings.lua
+++ dot_config/lvim/lua/bb/mappings.lua
@@ -83,30 +83,30 @@ M.setup = function()
   map("n", "<A-Left>", [[:vertical resize -2<CR>]])
   map("n", "<A-Right>", [[:vertical resize +2<CR>]])
 
-  vim.cmd [[
-    " https://github.com/tpope/vim-surround/issues/269
-    " Disable the default mappings (s, ys, S etc)
-    let g:surround_no_mappings=1
+  -- vim.cmd [[
+  --   " https://github.com/tpope/vim-surround/issues/269
+  --   " Disable the default mappings (s, ys, S etc)
+  --   let g:surround_no_mappings=1
 
-    nmap ds  <Plug>Dsurround
-    nmap cs  <Plug>Csurround
-    nmap cS  <Plug>CSurround
-    nmap s   <Plug>Ysurround
-    nmap yS  <Plug>YSurround
-    nmap ss  <Plug>Yssurround
-    nmap ySs <Plug>YSsurround
-    nmap ySS <Plug>YSsurround
-    xmap s   <Plug>VSurround
-    xmap gS  <Plug>VgSurround
+  --   nmap ds  <Plug>Dsurround
+  --   nmap cs  <Plug>Csurround
+  --   nmap cS  <Plug>CSurround
+  --   nmap s   <Plug>Ysurround
+  --   nmap yS  <Plug>YSurround
+  --   nmap ss  <Plug>Yssurround
+  --   nmap ySs <Plug>YSsurround
+  --   nmap ySS <Plug>YSsurround
+  --   xmap s   <Plug>VSurround
+  --   xmap gS  <Plug>VgSurround
 
-    if !exists("g:surround_no_insert_mappings") || ! g:surround_no_insert_mappings
-      if !hasmapto("<Plug>Isurround","i") && "" == mapcheck("<C-S>","i")
-        imap    <C-S> <Plug>Isurround
-      endif
-      imap      <C-G>s <Plug>Isurround
-      imap      <C-G>S <Plug>ISurround
-    endif
-  ]]
+  --   if !exists("g:surround_no_insert_mappings") || ! g:surround_no_insert_mappings
+  --     if !hasmapto("<Plug>Isurround","i") && "" == mapcheck("<C-S>","i")
+  --       imap    <C-S> <Plug>Isurround
+  --     endif
+  --     imap      <C-G>s <Plug>Isurround
+  --     imap      <C-G>S <Plug>ISurround
+  --   endif
+  -- ]]
 
   -- Alt+{k,j} drag line up/down
   -- Already provided by LunarVim
--- dot_config/lvim/lua/bb/plugins.lua
+++ dot_config/lvim/lua/bb/plugins.lua
@@ -15,9 +15,30 @@ M.setup = function()
     -- { "tpope/vim-vinegar",
     --   event = "BufRead",
     -- },
-    { "tpope/vim-surround",
-      event = "BufRead",
-    },
+    -- { "tpope/vim-surround",
+    --   'event' = "BufRead",
+    -- },
+      {
+        "kylechui/nvim-surround",
+        -- version = "*",  -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+          require('nvim-surround').setup({
+            keymaps = {
+              insert = "<C-g>s",
+              insert_line = "<C-g>S",
+              normal = "s",
+              normal_cur = "ss",
+              normal_line = "yS",
+              normal_cur_line = "ySS",
+              visual = "s",
+              visual_line = "gS",
+              delete = "ds",
+              change = "cs",
+            },
+          })
+        end
+      },
     { "tpope/vim-repeat",
       event = "BufRead",
     },
