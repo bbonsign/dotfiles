1cf02f341f0b22f1f8d182455636f616b4f38027
Brian Bonsignore <bonsignore.brian@gmail.com>
2024-07-22 13:41:12 -0400

    fix(nvim): vim-slime: default tmux config and fix custom key-binds

--- dot_config/nvim/lua/plugins/slime.lua
+++ dot_config/nvim/lua/plugins/slime.lua
@@ -1,13 +1,18 @@
 return {
   "jpalardy/vim-slime",
-  config = function()
-    vim.cmd([[let g:slime_target = "tmux"]])
-    vim.cmd([[let g:slime_bracketed_paste = 1]])
+  init = function()
+    vim.g.slime_target = "tmux"
+    vim.g.slime_bracketed_paste = 1
+    vim.g.slime_no_mappings = 1
+    -- https://github.com/jpalardy/vim-slime/blob/main/assets/doc/advanced.md#set-a-custom-default-config
+    vim.g.slime_dont_ask_default = 1
+    vim.g.slime_default_config = { socket_name = "default", target_pane = "right" }
   end,
 
   keys = {
-    { "<C-c>", "<Plug>SlimeMotionSend", desc = "Slime motion" },
-    { "<C-c><C-c>", "<Plug>SlimeLineSend", desc = "Slime line" },
     { "<C-c>", "<Plug>SlimeRegionSend", desc = "Slime region", mode = "x" },
+    { "<C-c><C-c>", "<Plug>SlimeLineSend", desc = "Slime line" },
+    { "<C-c>", "<Plug>SlimeMotionSend", desc = "Slime motion" },
+    -- { "<C-c>v", "<cmd>SlimeConfig<CR>", desc = "Slime config" },
   },
 }
