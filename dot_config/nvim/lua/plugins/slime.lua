70d511b39adb48aa2127282ec0d53d214892b940
Brian Bonsignore <bonsignore.brian@gmail.com>
2024-07-23 00:58:30 -0400

    feat(nvim): update vim-slime keybinds

--- dot_config/nvim/lua/plugins/slime.lua
+++ dot_config/nvim/lua/plugins/slime.lua
@@ -13,6 +13,14 @@ return {
     { "<C-c>", "<Plug>SlimeRegionSend", desc = "Slime region", mode = "x" },
     { "<C-c><C-c>", "<Plug>SlimeLineSend", desc = "Slime line" },
     { "<C-c>", "<Plug>SlimeMotionSend", desc = "Slime motion" },
-    -- { "<C-c>v", "<cmd>SlimeConfig<CR>", desc = "Slime config" },
+    { "<C-c><C-v>", "<cmd>SlimeConfig<CR>", desc = "Slime config" },
+
+    -- Conviences to target tmux panes in different directions
+    -- Once executed, the keybinds above will target the new pane until changed again.
+    { "<C-c><C-h>", [[:exec 'let g:slime_default_config.target_pane = "{left-of}"'<CR>]]  , silent=true},
+    { "<C-c><C-j>", [[:exec 'let g:slime_default_config.target_pane = "{down-of}"'<CR>]]  , silent=true},
+    { "<C-c><C-k>", [[:exec 'let g:slime_default_config.target_pane = "{up-of}"'<CR>]]    , silent=true},
+    { "<C-c><C-l>", [[:exec 'let g:slime_default_config.target_pane = "{right-of}"'<CR>]] , silent=true},
+
   },
 }
