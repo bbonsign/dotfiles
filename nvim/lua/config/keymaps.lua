1ab3163c0692035d6be58c2e8f2cf1908acaa2df
Brian Bonsignore <bonsignore.brian@gmail.com>
2025-02-14 16:53:20 -0500

    feat(nvim): keymap <leader>p to paste from register 0

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -31,6 +31,9 @@ vim.keymap.set("n", "<C-h>", "<C-e>")
 vim.keymap.set("n", "<C-j>", "<C-e>")
 vim.keymap.set("n", "<C-k>", "<C-y>")
 
+vim.keymap.set("n", "<Leader>p", '"0p', { desc = "Paste last yank" })
+vim.keymap.set("n", "<Leader>P", '"0P', { desc = "Paste last yank" })
+
 vim.keymap.set("n", "<Leader>=", ":.lua<CR>", { desc = "Lua eval" })
 vim.keymap.set("n", "g=", ":.lua<CR>", { desc = "Lua eval" })
 vim.keymap.set("v", "<Leader>=", ":lua<CR>", { desc = "Lua eval" })
